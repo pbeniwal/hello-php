4da01bdb4af1dca367741baaf3e7651b229b4ac5
edurekaproj <edurekaproj@gmail.com>
2021-05-24 11:20:20 +0530

    updated

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
 
-echo "Hello World from Azure Essentials Masterclass Day 3 for JECRC University on 24-May-2021 for Final year students";
+echo "Hello World from Azure Essentials Masterclass Day 3 for JECRC University on 24-May-2021 for Final year students!!!";
