0368a24c8c3af506a60eec8e46df678218e5f29b
pbeniwal <pbeniwal@yahoo.com>
2021-05-24 11:17:05 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
 
-echo "Hello World from Azure Eseentials Masterclass on 24-May-2021";
+echo "Hello World from Azure Essentials Masterclass Day 3 for JECRC University on 24-May-2021 for Final year students";
