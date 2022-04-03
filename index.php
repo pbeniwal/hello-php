a21d1b06741550a600e37fef3e36ba2f23871b2b
pbeniwal <pbeniwal@yahoo.com>
2022-04-03 07:10:53 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
 
-echo "Hello World from Azure Essentials Masterclass Day 3 for Amity University on 03-Apr-2022 10:0 AM!!!";
+echo "Hello World from Azure Essentials Masterclass Day 3 for Amity University on 03-Apr-2022 10:00 AM!!!";
